d5a9e13d102a6d544413dc1a718f3d277a8eb62b
801950 <801950@unizar.es>
2022-03-27 14:38:04 -0700

    api completo

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/AuthController.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/AuthController.java
@@ -1,9 +1,9 @@
 package com.cerea_p1.spring.jpa.postgresql.controller;
 
-import java.util.HashSet;
+/* import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.stream.Collectors; */
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -72,33 +72,6 @@ public class AuthController {
 		Usuario user = new Usuario(signUpRequest.getUsername(), 
 							 signUpRequest.getEmail(),
 							 encoder.encode(signUpRequest.getPassword()),signUpRequest.getPais());
-//		Set<String> strRoles = signUpRequest.getRole();
-//		Set<Role> roles = new HashSet<>();
-		// if (strRoles == null) {
-		// 	Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-		// 			.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-		// 	roles.add(userRole);
-		// } else {
-		// 	strRoles.forEach(role -> {
-		// 		switch (role) {
-		// 		case "admin":
-		// 			Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-		// 					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-		// 			roles.add(adminRole);
-		// 			break;
-		// 		case "mod":
-		// 			Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
-		// 					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-		// 			roles.add(modRole);
-		// 			break;
-		// 		default:
-		// 			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
-		// 					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
-		// 			roles.add(userRole);
-		// 		}
-		// 	});
-		// }
-		// user.setRoles(roles);
 		userRepository.save(user);
 		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
 	}
