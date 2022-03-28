99ce2d17a659e1d8fefc52eeb3486ce947bdbb8c
801950 <801950@unizar.es>
2022-03-28 01:20:16 -0700

    adding sesion

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/security/WebSecurityConfig.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/security/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable()
 			.exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
 			.anyRequest().authenticated();
