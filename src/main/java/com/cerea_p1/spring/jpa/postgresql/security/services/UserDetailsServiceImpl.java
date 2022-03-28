44ca91fd817acf633cfd3c327aaeaf426330847f
801950 <801950@unizar.es>
2022-03-28 15:37:32 -0700

    lalal

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/security/services/UserDetailsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
 import com.cerea_p1.spring.jpa.postgresql.repository.UsuarioRepository;
 
-@Service
+@Service("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
   @Autowired
   UsuarioRepository userRepository;
