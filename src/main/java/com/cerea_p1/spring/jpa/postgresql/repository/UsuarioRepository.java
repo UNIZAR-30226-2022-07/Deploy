e22e7ec1b2af0d54cfd93b26334b311f542d39d2
801950 <801950@unizar.es>
2022-03-28 15:30:27 -0700

    lalal

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/repository/UsuarioRepository.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/repository/UsuarioRepository.java
@@ -3,9 +3,7 @@ import java.util.Optional;
 import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,String> {
     Optional<Usuario> findByUsername(String username);
     Boolean existsByUsername(String username);
