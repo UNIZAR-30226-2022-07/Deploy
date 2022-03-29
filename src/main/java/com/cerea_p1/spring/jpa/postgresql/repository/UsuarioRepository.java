55a6396f914f52c93404a60776ddcf35fa1323ec
801950 <801950@unizar.es>
2022-03-29 02:29:30 -0700

    lalal

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/repository/UsuarioRepository.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.cerea_p1.spring.jpa.postgresql.repository;
 
+import java.util.Optional;
 import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,23 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,String> {
-    Usuario findByUsername(String username);
+    Optional<Usuario> findByUsername(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
-
-
-
-
-// import java.util.Optional;
-// import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// @Repository
-// public interface UsuarioRepository extends JpaRepository<Usuario,String> {
-//     Optional<Usuario> findByUsername(String username);
-//     Boolean existsByUsername(String username);
-//     Boolean existsByEmail(String email);
-// }
--- src/main/java/com/cerea_p1/spring/jpa/postgresql/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/security/services/UserDetailsServiceImpl.java
@@ -1,25 +1,25 @@
 package com.cerea_p1.spring.jpa.postgresql.security.services;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
-// import org.springframework.security.core.userdetails.UsernameNotFoundException;
-// import org.springframework.stereotype.Service;
-// import org.springframework.transaction.annotation.Transactional;
-// import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
-// import com.cerea_p1.spring.jpa.postgresql.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
+import com.cerea_p1.spring.jpa.postgresql.repository.UsuarioRepository;
 
-// @Service
-// public class UserDetailsServiceImpl implements UserDetailsService {
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
 
-//   @Autowired
-//   UsuarioRepository userRepository;
+  @Autowired
+  UsuarioRepository userRepository;
 
-//   @Override
-//   @Transactional
-//   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//     Usuario user = userRepository.findByUsername(username)
-//         .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
-//     return UserDetailsImpl.build(user);
-//   }
-// }
+  @Override
+  @Transactional
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Usuario user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+    return UserDetailsImpl.build(user);
+  }
+}
