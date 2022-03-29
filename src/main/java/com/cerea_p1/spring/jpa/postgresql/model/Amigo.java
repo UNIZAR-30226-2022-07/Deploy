cae5c11af07cdfdf51729ee6c77b55d68f08c213
801950 <801950@unizar.es>
2022-03-29 15:50:28 -0700

    fiend add

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/model/Amigo.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/model/Amigo.java
@@ -3,7 +3,6 @@ package com.cerea_p1.spring.jpa.postgresql.model;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 import java.util.*;
-import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
 
 @Entity
 @Table(name = "amigo", 
@@ -14,13 +13,13 @@ import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
 public class Amigo {
     @Id
     @Column(name = "usuario1", nullable = false, length = 255)
-    private Usuario usuario1;
+    private String usuario1;
     @Id
     @Column(name = "usuario2", nullable = false, length = 255)
-    private Usuario usuario2;
+    private String usuario2;
     
 
-    public Amigo(Usuario u, Usuario u2) {
+    public Amigo(String u, String u2) {
         usuario1 = u;
         usuario2 = u2;
     }
@@ -28,24 +27,24 @@ public class Amigo {
     public Amigo() {
     }
 
-    public Usuario getUsuario1(){
-            return usuario1;
-        }
+    public String getUsuario1(){
+        return usuario1;
+    }
 
-    public void setUsuario1(Usuario u){
+    public void setUsuario1(String u){
         this.usuario1 = u;
     }
 
-    public Usuario getUsuario2(){
+    public String getUsuario2(){
         return usuario2;
     }
 
-    public void setUsuario2(Usuario u){
+    public void setUsuario2(String u){
         this.usuario2 = u;
     }
 
     @Override
     public String toString(){
-        return "[username= " + this.usuario1.getUsername() + "friendname= " + this.usuario2.getUsername() + "]";
+        return "[username= " + this.usuario1 + "friendname= " + this.usuario2 + "]";
     }
 }
