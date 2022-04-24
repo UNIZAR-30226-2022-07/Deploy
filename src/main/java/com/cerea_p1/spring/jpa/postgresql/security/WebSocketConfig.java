9524f25a8e6dd1f794b631ad0653b1a9ccead785
801950 <801950@unizar.es>
2022-04-24 03:05:37 -0700

    adding websocket security

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/security/WebSocketConfig.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/security/WebSocketConfig.java
@@ -2,16 +2,36 @@ package com.cerea_p1.spring.jpa.postgresql.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import com.cerea_p1.spring.jpa.postgresql.security.websocket.WebSocketHandler;
 
 
 
 @Configuration
+//@EnableWebSocketMessageBroker
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@Deprecated
+public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages.simpDestMatchers("/secured/**", "/secured/**/**").authenticated().anyMessage().authenticated();
+    }
+
+ /*   @Override 
+    protected void configureInbound(
+    MessageSecurityMetadataSourceRegistry messages) { 
+        messages
+        .simpDestMatchers("/secured/**").authenticated()
+        .anyMessage().authenticated(); 
+    }*/
  
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -24,9 +44,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
      //   registry.addEndpoint("/broadcast");  // it is OK to leave it here
         // registry.addEndpoint("/broadcast").withSockJS();
         // custom heartbeat, every 60 sec
-        registry.addEndpoint("/onep1-game")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+        //registry.addEndpoint("/onep1-game").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/onep1-game").withSockJS();
+
         
     }
 }
