cd71862725f882726686ed7b8277c2795a68dd17
saladito <cesarborjamoreno@gmail.com>
2022-04-29 19:28:29 +0200

    cositas

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/FriendController.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/FriendController.java
@@ -68,7 +68,10 @@ public class FriendController {
 					logger.info("Usuarios encontrados " + user + " " + user2 );
 					InvitacionAmistad inv = new InvitacionAmistad(user,user2);
 					logger.info("Se crea el objeto" );
-					invitacionRepository.save(inv);
+					user.addInvitacionesEnviadas(user2);
+					user2.addInvitacion(user);
+					userRepository.save(user);
+					userRepository.save(user2);
 					return ResponseEntity.ok(new MessageResponse("Petición de amistad enviada a " + user2.getUsername()));
 				} else return ResponseEntity.badRequest().body(new MessageResponse("Error: No se puede enviar la petición de amistad"));
 			} else return ResponseEntity.badRequest().body(new MessageResponse("Error: No se puede enviar la petición de amistad"));
