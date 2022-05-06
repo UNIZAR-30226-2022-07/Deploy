5b7260db045800d6fc548b6997e01e362b86bd66
nerea <801950@unizar.es>
2022-05-06 13:28:45 -0700

    prueba ranking amigos

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/RankingController.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/RankingController.java
@@ -63,14 +63,14 @@ public class RankingController {
         }
 	}
 
-    // @PostMapping("/rankingAmigos")
-    // @MessageExceptionHandler()
-	// public ResponseEntity<?> rankingPorAmigos(@RequestBody RankingAmigosRequest amigosRequest) {
-    //     try{
-    //         return ResponseEntity.ok(new MessageResponse(Sender.enviar(userRepository.userRankingAmigos(amigosRequest.getUsername()))));
-    //     }catch (Exception e){
-    //         logger.warning("Exception" + e.getMessage());
-    //         return ResponseEntity.badRequest().body(Sender.enviar(e.getMessage()));
-    //     }
-	// }
+    @PostMapping("/rankingAmigos")
+    @MessageExceptionHandler()
+	public ResponseEntity<?> rankingPorAmigos(@RequestBody RankingAmigosRequest amigosRequest) {
+        try{
+            return ResponseEntity.ok(new MessageResponse(Sender.enviar(userRepository.userRankingAmigos(amigosRequest.getUsername()))));
+        }catch (Exception e){
+            logger.warning("Exception" + e.getMessage());
+            return ResponseEntity.badRequest().body(Sender.enviar(e.getMessage()));
+        }
+	}
 }
--- src/main/java/com/cerea_p1/spring/jpa/postgresql/repository/UsuarioRepository.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/repository/UsuarioRepository.java
@@ -18,6 +18,6 @@ public interface UsuarioRepository extends JpaRepository<Usuario,String> {
     List<String> userRankingByPais(@Param("pais") String pais);
     @Query("SELECT username, puntos, pais FROM Usuario u ORDER BY puntos DESC")
     List<String> userRankingMundial();
-    // @Query("SELECT u.username, u.puntos, u.pais FROM Amigo a INNER JOIN a.usuario2 u WHERE :username = a.usuario1 ORDER BY puntos DESC")
-    // List<String> userRankingAmigos(@Param("username") String username);
+    @Query("SELECT u.username, u.puntos, u.pais FROM Usuario u, Amigo a WHERE :username = a.usuario1 and a.usuario2 = u.username ORDER BY puntos DESC")
+    List<String> userRankingAmigos(@Param("username") String username);
 }
