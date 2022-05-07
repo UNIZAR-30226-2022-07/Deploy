4dfd410d163500d3edea229bef48bdf17342380b
nerea <801950@unizar.es>
2022-05-07 14:50:11 -0700

    prueba restablecer contraseña

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/AuthController.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/AuthController.java
@@ -143,21 +143,21 @@ public class AuthController {
 		
 	}
 
-	// @Bean
-	// public JavaMailSenderImpl getJavaMailSender() {
-	// 	JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	// 	mailSender.setHost("smtp.gmail.com");
-	// 	mailSender.setPort(587);
+	@Bean
+	public JavaMailSenderImpl getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
 		
-	// 	mailSender.setUsername("onep1game@gmail.com");
-	// 	mailSender.setPassword("*onep1-Game");
+		mailSender.setUsername("onep1game@gmail.com");
+		mailSender.setPassword("*onep1-Game");
 		
-	// 	Properties props = mailSender.getJavaMailProperties();
-	// 	props.put("mail.transport.protocol", "smtp");
-	// 	props.put("mail.smtp.auth", "true");
-	// 	props.put("mail.smtp.starttls.enable", "true");
-	// 	props.put("mail.debug", "true");
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
 		
-	// 	return mailSender;
-	// }
+		return mailSender;
+	}
 }
