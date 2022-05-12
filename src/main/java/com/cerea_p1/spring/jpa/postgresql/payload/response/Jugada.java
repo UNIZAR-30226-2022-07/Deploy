77ce8833647183da7e61e7d3be76f654f544a4c2
nerea <801950@unizar.es>
2022-05-12 04:13:10 -0700

    jugar una carta

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/payload/response/Jugada.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/payload/response/Jugada.java
@@ -3,12 +3,12 @@ package com.cerea_p1.spring.jpa.postgresql.payload.response;
 import com.cerea_p1.spring.jpa.postgresql.model.game.Carta;
 import com.cerea_p1.spring.jpa.postgresql.model.game.Jugador;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Jugada {
     private Carta carta;
-    private List<InfoJugador> jugadores;    
+    private List<InfoJugador> jugadores = new ArrayList<InfoJugador>();    
     
     public Jugada(Carta c, List<Jugador> j){
         carta = c;
