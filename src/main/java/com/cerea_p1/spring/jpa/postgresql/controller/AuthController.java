e86fadcafe7c37d42e810bb9910177d863c9c15e
saladito <cesarborjamoreno@gmail.com>
2022-05-18 20:16:32 +0200

    intento tiempo turnos

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/AuthController.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/AuthController.java
@@ -55,7 +55,7 @@ public class AuthController {
 
 	@Autowired
 	private JavaMailSender javaMailSender;
-	@PostMapping("/signin")
+	@PostMapping(value = "/signin")
 	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 		
 		Authentication authentication = authenticationManager.authenticate( 
