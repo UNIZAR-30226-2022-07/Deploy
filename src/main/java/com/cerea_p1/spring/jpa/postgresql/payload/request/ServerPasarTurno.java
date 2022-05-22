a2affb6146082a50a3af3716bd36af5c43c4127c
nerea <801950@unizar.es>
2022-05-22 12:05:19 -0700

    error

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/payload/request/ServerPasarTurno.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/payload/request/ServerPasarTurno.java
@@ -1,12 +1,6 @@
 package com.cerea_p1.spring.jpa.postgresql.payload.request;
 
-import java.util.List;
-
 import javax.validation.constraints.NotBlank;
-
-import com.cerea_p1.spring.jpa.postgresql.model.game.Carta;
-import com.cerea_p1.spring.jpa.postgresql.model.game.Jugador;
-
 
 public class ServerPasarTurno {
     @NotBlank
