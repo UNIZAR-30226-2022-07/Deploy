58a805088d9588b348f81225215b4e29aa2e8376
nerea <801950@unizar.es>
2022-05-24 05:46:58 -0700

    prueba ranking amigos

--- src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/RankingController.java
+++ src/main/java/com/cerea_p1/spring/jpa/postgresql/controller/RankingController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cerea_p1.spring.jpa.postgresql.model.Usuario;
 import com.cerea_p1.spring.jpa.postgresql.payload.request.rankings.RankingAmigosRequest;
 import com.cerea_p1.spring.jpa.postgresql.payload.request.rankings.RankingPaisRequest;
 import com.cerea_p1.spring.jpa.postgresql.payload.response.MessageResponse;
@@ -19,7 +20,10 @@ import com.cerea_p1.spring.jpa.postgresql.security.jwt.JwtUtils;
 
 import com.cerea_p1.spring.jpa.postgresql.utils.Sender;
 
-
+import java.lang.StackWalker.Option;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.logging.*;
 
 
@@ -61,7 +65,22 @@ public class RankingController {
 	public ResponseEntity<?> rankingPorAmigos(@RequestBody RankingAmigosRequest amigosRequest) {
         logger.info("user = " + amigosRequest.getUsername());
         try{
-            return ResponseEntity.ok(new MessageResponse(Sender.enviar(userRepository.userRankingAmigos(amigosRequest.getUsername()))));
+            List<String> l1 = userRepository.userRankingAmigos(amigosRequest.getUsername());
+            List<String> l2 = new ArrayList<String>();
+            Optional<Usuario> opU = userRepository.findByUsername(amigosRequest.getUsername());
+            Usuario u = opU.get();
+            if(l1.size() > 0){
+                for(String s : l1){
+                    String[] textElements = s.split(",");
+                    if(Integer.valueOf(textElements[1]) < u.getPuntos()){
+                        l2.add(u.getUsername()+","+u.getPuntos()+","+u.getPais());
+                    }
+                    l2.add(s);
+                }
+            } else {
+                l2.add(u.getUsername()+","+u.getPuntos()+","+u.getPais());
+            }
+            return ResponseEntity.ok(new MessageResponse(Sender.enviar(l2)));
         }catch (Exception e){
             logger.warning("Exception" + e.getMessage());
             return ResponseEntity.badRequest().body(Sender.enviar(e.getMessage()));
